878e12f12f55f4931e891c92a0ad1d7811c25314
minkyokyo0807 <minkyokyo0807@gmail.com>
2021-03-27 01:51:10 +0900

    springboot-initializied

---
+++ diary_web/src/main/java/com/diary_web/diary_web/DiaryWebApplication.java
@@ -0,0 +1,13 @@
+package com.diary_web.diary_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryWebApplication.class, args);
+	}
+
+}
---
+++ diary_web/src/test/java/com/diary_web/diary_web/DiaryWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary_web.diary_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
